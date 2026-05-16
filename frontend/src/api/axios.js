55e24bb8e0716e4ae09aa6b430c09f847ca54931
machagiri-max <nirudumachagiri0@gmail.com>
2026-05-16 20:02:11 +0530

    Fix API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api'
+
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: BASE_URL,
 })
 
 api.interceptors.request.use(function(cfg) {
@@ -14,7 +16,7 @@ api.interceptors.response.use(function(res) { return res }, async function(err) 
   if (err.response && err.response.status === 401) {
     try {
       const refresh = localStorage.getItem('refresh')
-      const response = await axios.post('http://localhost:8000/api/auth/token/refresh/', { refresh: refresh })
+      const response = await axios.post(BASE_URL + '/auth/token/refresh/', { refresh: refresh })
       localStorage.setItem('access', response.data.access)
       err.config.headers.Authorization = 'Bearer ' + response.data.access
       return api(err.config)
